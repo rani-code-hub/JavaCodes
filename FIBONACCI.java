eb27e79281519643cfeebe1c5263bad822bb85c0
rani-code-hub <srijitasur11b1@gmail.com>
2026-03-24 00:22:10 +0530

    Add files via upload

---
+++ FIBONACCI.java
@@ -0,0 +1,13 @@
+public class FIBONACCI {
+    public static void main(String[] args) {
+        int a=0;
+        int b=1,c;
+        System.out.print(a+" "+b);
+        for(int i=2;i<=10;i++){
+            c=a+b;
+            System.out.print(c);
+            a=b;
+            b=c;
+        }
+    }
+}
